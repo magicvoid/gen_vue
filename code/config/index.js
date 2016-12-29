ca0fb51db228751f03bddb43e45d4785cf23fc81
陈星凌 <magicvoid@me.com>
2016-12-29 10:19:20 +0800

    Revert "config build for Java server"
    This reverts commit 68f37b419512230fe99ac1880b1faaccd2c98cef.

--- code/config/index.js
+++ code/config/index.js
@@ -4,10 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../../../gen/src/main/webapp/WEB-INF/views/index/index.html'),
-    assetsRoot: path.resolve(__dirname, '../../../gen/src/main/webapp'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/alpha/',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
